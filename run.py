0189cfa308c797ed34987892250912c71b270bbf
Bohdan Karvatskyi <bohdan.karvatskyi@cryptaldash.com>
2018-11-17 18:50:05 +0200

    turn off posting

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from telegram_bot import updater
 from thread_svc import start_schedule, run_threaded, posting_messages
 
 
-schedule.every().second.do(posting_messages)
-run_threaded(start_schedule)
+# schedule.every().second.do(posting_messages)
+# run_threaded(start_schedule)
 
 updater.start_polling()
